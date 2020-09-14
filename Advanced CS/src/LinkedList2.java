27793839ff625aa0ec7cd845a5701d17e16f5c6f
davidfriedman <davidfriedman@localhost>
2020-09-14 12:03:46 -0400

    Correct LinkedList2 file

--- Advanced CS/src/LinkedList2.java
+++ Advanced CS/src/LinkedList2.java
@@ -1,4 +1,36 @@
+import java.util.AbstractList;
 
-public class LinkedList2 {
+public class LinkedList2<T> extends AbstractList<T> {
+	
+	
+	private class Node {
+		
+		private Node next;
+		private T info;
+		
+		
+		public Node(T info) {
+			this.info = info;
+		}
+		
+		public Node (T info, Node next) {
+			this.next = next;
+			this.info = info;
+		}
+	}
+	
+	
+	
 
+	@Override
+	public Object get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
