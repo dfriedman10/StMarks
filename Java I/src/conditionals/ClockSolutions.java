d8832a0594e2e44ac4eb0b8d3aa1df12df7790f8
davidfriedman <davidfriedman@localhost>
2020-10-05 11:46:53 -0400

    clock solutions

---
+++ Java I/src/conditionals/ClockSolutions.java
@@ -0,0 +1,36 @@
+package conditionals;
+public class ClockSolutions {
+	private int minutes;
+	
+	public void setTime(int min) {
+		minutes = min;
+	}
+	
+	public void tick() {
+		minutes = (minutes+1)%60;
+	}
+	public void displayTime() {
+		System.out.println(minutes);
+	}
+	
+	public static void main(String[] args) {
+		ClockSolutions tester = new ClockSolutions();
+		
+		// should display the time to be 57, 58, 59, 0, 1, etc.
+		tester.setTime(56);
+		tester.tick();
+		tester.displayTime();
+		tester.tick();
+		tester.displayTime();
+		tester.tick();
+		tester.displayTime();
+		tester.tick();
+		tester.displayTime();
+		tester.tick();
+		tester.displayTime();
+		tester.tick();
+		tester.displayTime();
+		tester.tick();
+		tester.displayTime();
+	}
+}
