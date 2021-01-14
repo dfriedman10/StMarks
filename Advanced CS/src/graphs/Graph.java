0640f5d79123a3a524f3cdbdaec6734849538f7d
davidfriedman <davidfriedman@localhost>
2021-01-14 08:43:14 -0500

    Updated Graph

--- Advanced CS/src/graphs/Graph.java
+++ Advanced CS/src/graphs/Graph.java
@@ -1,4 +1,5 @@
 package graphs;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
@@ -33,6 +34,57 @@ public class Graph<E> {
 		public Vertex(E info) {
 			this.info = info;
 			neighbors = new HashSet<Vertex>();
-		}	
+		}
+	}
+	
+	
+	public void BFS(E start, E target) {
+		
+		ArrayList<Vertex> toVisit = new ArrayList<Vertex>();
+		toVisit.add(vertices.get(start));
+		HashSet<Vertex> visited = new HashSet<Vertex>();
+		visited.add(vertices.get(start));
+		
+		HashMap<Vertex, Vertex> leadsTo = new HashMap<Vertex, Vertex>();
+		
+		while (!toVisit.isEmpty()) {
+			
+			Vertex curr = toVisit.remove(0);
+			
+			for (Vertex neighbor : curr.neighbors) {
+				
+				if (visited.contains(neighbor)) continue;
+				
+				if (neighbor.info.equals(target)) {
+					
+					//backTrace(target, start, leadsTo);
+					return;
+				}
+				
+				else {
+					toVisit.add(neighbor);
+					visited.add(neighbor);
+					leadsTo.put(neighbor, curr);
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		Graph<String> g = new Graph<String>();
+		
+		g.addVertex("Reina");
+		g.addVertex("Veronika");
+		g.addVertex("Felicity");
+		g.addVertex("Andria");
+		g.addVertex("Elgin");
+		
+		g.connect("Reina","Veronika");
+		g.connect("Reina", "Felicity");
+		g.connect("Felicity", "Andria");
+		g.connect("Felicity", "Elgin");
+		
+		g.BFS("Reina", "Elgin");
 	}
 }
